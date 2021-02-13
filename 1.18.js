5d56b9cc3d92d084b1f4eb5be22e31498f897da4
rociobastiasanchez <rociobastiasanchez@gmail.com>
2021-02-13 12:16:19 +0100

    actualizadp

--- 1.18.js
+++ 1.18.js
@@ -3,7 +3,7 @@
 
 const enlaces = document.getElementsByTagName("a");
 console.log(enlaces);
-console.log(`Hay ${enlaces.length}$ enlaces`);
+console.log(`Hay ${enlaces.length} enlaces`);
 
 //Direccion a la que enlaza el penultimo enlace
 
@@ -19,7 +19,18 @@ const enlacesTercer = tercer.getElementsByTagName("a");
 
 console.log(enlacesTercer.length);
 
+//Insertar un nuevo contenido en un párrafo creado en HTML
 const resultado = document.getElementById("resultado");
+//Con innerHTML lo vinculamos
 resultado.innerHTML = enlacesTercer.length;
 
+//Ahora insertamos un nuevo párrafo en HTML desde js
 
+//Crear un nodo tipo Elemento
+const parrafo = document.createElement ('p');
+//Crear el nodo tipo Text
+const contenido = document.createTextNode('Este es un nuevo párrafo');
+//Añadir el nodo Text como hijo del nodo Element
+parrafo.appendChild(contenido);
+//Añadir el nodo Element como hijo de la página
+document.body.appendChild(parrafo);
